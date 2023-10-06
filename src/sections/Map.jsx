f52c637f303124f0f4d612d9dfb1169383e7c39c
IhorSliepchenko1 <igorslepchenko.cc@gmail.comD>
2023-10-06 13:11:32 +0300

    map

--- src/sections/Map.jsx
+++ src/sections/Map.jsx
@@ -1,7 +1,18 @@
+import GoogleMapReact from "google-map-react";
+
 export const Map = () => {
+  const defaultProps = {
+    center: { lat: 37.7749, lng: -122.4194 },
+    zoom: 10, 
+  };
+
   return (
-    <>
-      <h1 className="map">MAP</h1>
-    </>
+    <div className="map container">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyDxp7tD50n5Zm3NovmleG-HvSo-iVFEyG0" }} // Ваш ключ Google Maps API
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      ></GoogleMapReact>
+    </div>
   );
 };
