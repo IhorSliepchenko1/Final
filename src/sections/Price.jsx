d41c157381be9b301a90193f6fdd04b0e5d31863
Isaeveg <isaev.egor.08@gmail.com>
2023-10-07 17:07:10 +0300

    init repo

--- src/sections/Price.jsx
+++ src/sections/Price.jsx
@@ -1,7 +1,28 @@
+
 export const Price = () => {
   return (
     <>
-      <h1 className="price">Price</h1>
+      <section className="price">
+        <div className="price__header">
+          <h2 className="price__title"></h2>
+          <p className="price__desc"></p>
+        </div>
+        <div className="price__container">
+          <div className="price__item plan">
+            <h3 className="plan__title"></h3>
+            <div className="plan__prices">
+              <h2 className="plan__subtitle"></h2>
+              <span className="plan__hour"></span>
+            </div>
+            <p className="plan__desc"></p>
+
+            <ul className="plan__ul">
+              <li className="plan__item">
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
