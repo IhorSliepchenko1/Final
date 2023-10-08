d29c65904563fba7a91e67b37967f8596541fcd3
Isaeveg <isaev.egor.08@gmail.com>
2023-10-08 17:33:16 +0300

    init

--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -21,6 +21,56 @@ export const Portfolio = () => {
       }
     });
   }
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     orders: [],
+  //     items: [
+  //       {
+  //         id:1,
+  //         img: '../assets/img/2.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:2,
+  //         img: '../assets/img/3.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:3,
+  //         img: '../assets/img/4.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:4,
+  //         img: '../assets/img/5.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:5,
+  //         img: '../assets/img/6.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:6,
+  //         img: '../assets/img/7.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:7,
+  //         img: '../assets/img/8.png',
+  //         category: 'ui',
+  //       },
+  //       {
+  //         id:8,
+  //         img: '../assets/img/9.png',
+  //         category: 'ui',
+  //       },
+  //     ]
+  //   }
+  // }
+
   return (
     <>
       <section className="portfolio">
